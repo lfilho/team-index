91f7f09c19d6a44e47201037bbde9f1ded5e9d85
Josh Johnston <josh@x-team.com>
2015-04-09 19:47:00 +1000

    refactored

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let Fingdex = require('fingdex');
+let fs = require('fs');
 let bind = require('bind-fn');
 let arrayFrom = require('./lib/array-from');
 
@@ -14,78 +15,14 @@ if (!sourceFilename || !teamId || !ts) {
   process.exit();
 }
 
-// ----
-// create a source for log entries
-
 let sourceIndex = new Fingdex();
-let fs = require('fs');
 fs.readFileSync(sourceFilename, 'utf8')
   .split('\n')
   .filter(Boolean)
   .map(JSON.parse)
   .forEach(bind(sourceIndex, 'addEntry'));
 
-// ----
-// create an index that will aggregate entries into docs
-
-let docDefaults = {};
-
-docDefaults.team = {
-  startedAt: 0
-};
-
-docDefaults.teamMembership = {
-  startedAt: 0,
-  hoursPerWeek: 40
-};
-
-let docIndex = require('fingdex/lib/doc-index')(sourceIndex, docDefaults);
-
-// ----
-// create an index that infer a timeline of events from docs
-
-
-let timelineHandlers = {};
-
-timelineHandlers.team = function (entry) {
-  // for teams we record events of when the team started or ended
-  return ['startedAt', 'endedAt'];
-};
-
-timelineHandlers.teamMembership = function (entry) {
-  // for team memberships we record events of when the membership started or ended
-  return ['startedAt', 'endedAt'];
-};
-
-let timelineIndex = require('./lib/timeline-index')(docIndex, timelineHandlers);
-
-// ----
-
-function showTeamAtTime (teamId, now, cb) {
-  let teamsAtTime = require('./lib/team-at-time')(docIndex, timelineIndex);
-  timelineIndex.db.readStream({
-    keys: false,
-    lt: [now]
-  })
-    .on('data', bind(teamsAtTime, 'processEvent'))
-    .on('close', function () {
-      let sum = function (acc, val) {
-        return (acc || 0) + val;
-      };
-
-      let hours = teamsAtTime.teams.memberHoursByTeam.get(teamId) || new Map();
-      let totalHours = arrayFrom(hours.values()).reduce(sum, 0);
-      let members = teamsAtTime.teams.membersByTeam.get(teamId) || new Set();
-
-      console.log('\nWho is in the %s team @ %s?\n', teamId, new Date(now), arrayFrom(members));
-      console.log('Total dev hours per week: %d\n', totalHours, arrayFrom(hours));
-
-      if (cb) { cb(); }
-    });
-}
-
-// ----
-
-timelineIndex.catchup();
-console.log('\nInfo about %s:', teamId, docIndex.getDoc(teamId));
-showTeamAtTime(teamId, ts);
+require('./lib/get-timeline-data')(sourceIndex, teamId, ts, function (err, res) {
+  console.log('\nWho is in the %s team @ %s?\n', teamId, new Date(ts), arrayFrom(res.members));
+  console.log('Total dev hours per week: %d\n', res.totalHours, arrayFrom(res.hours));
+});
