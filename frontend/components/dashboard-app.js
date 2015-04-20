58781b8de70c0ac025ae6786fd5729935097c230
Josh Johnston <josh@x-team.com>
2015-04-20 12:46:48 +1000

    renamed app -> dashboard-app

---
+++ frontend/components/dashboard-app.js
@@ -0,0 +1,45 @@
+var React = require('react');
+
+function setup (createConnectedComponent) {
+  var MainContent = createConnectedComponent(require('./main-content'), ['auth', 'route'], function (stores, props) {
+    return {
+      isLoggedIn: stores.auth.isLoggedIn(),
+      route: stores.route.data.current
+    };
+  });
+
+  var AuthControls = createConnectedComponent(require('./auth-controls'), ['auth'], function (stores, props) {
+    return stores.auth.get();
+  });
+
+  var Menu = createConnectedComponent(require('./menu'), ['auth', 'route'], function (stores, props) {
+    return {
+      isLoggedIn: stores.auth.isLoggedIn(),
+      route: stores.route.data.current
+    };
+  });
+
+  return React.createClass({
+      render: function () {
+        console.log('render: DashboardApp', this.props);
+
+        return (
+          <div>
+            <header>
+              <div className="menu">
+                <Menu />
+              </div>
+              <div className="user">
+                <AuthControls />
+              </div>
+            </header>
+            <div className="main">
+              <MainContent />
+            </div>
+          </div>
+        );
+      }
+    });
+};
+
+module.exports = setup;
