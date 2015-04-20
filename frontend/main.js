fcd42c64aae8f140d12ae0d80216be9d57b05607
Josh Johnston <josh@x-team.com>
2015-04-20 12:47:41 +1000

    rejigging components / stores / actions

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,4 @@
 var React = require('react');
-var DashboardApp = require('./components/app');
 
 /*
 
@@ -9,11 +8,64 @@ var DashboardApp = require('./components/app');
   `window.authState` is set in the page template, containing info about the current user's authentication.
 
 */
-var actionHandler = require('./lib/action-handler')({
-  auth: window.authState || {}
+
+var Store = require('./lib/store');
+
+var stores = {};
+
+// ----
+
+stores.auth = new Store({
+  name: window.authState.name,
+  picture: window.authState.picture
 });
 
+stores.auth.isLoggedIn = function () {
+  return !!this.data.name;
+};
+
+// ----
+
+stores.route = new Store({
+  current: '/'
+});
+
+// ----
+
+var actions = {};
+
+actions.login = function (args) {
+  location.href = '/login';
+};
+
+var xhr = require('xhr');
+actions.logout = function () {
+  xhr({
+    uri: '/logout',
+  }, function (err) {
+    if (err) { return cb(err); }
+    if (resp.statusCode !== 200) { console.error('logout failed'); }
+
+    stores.auth.set({
+      name: null,
+      picture: null
+    });
+  });
+};
+
+actions.setRoute = function (args) {
+  stores.route.set({
+    current: args.route
+  });
+};
+
+var createConnectedComponent = require('./lib/create-connected-component')(stores, actions);
+var DashboardApp = require('./components/dashboard-app')(createConnectedComponent);
+
 React.render(
-  React.createElement(DashboardApp, { actionHandler: actionHandler }),
+  React.createElement(DashboardApp),
   document.getElementById('app')
 );
+
+window.stores = stores;
+window.actions = actions;
