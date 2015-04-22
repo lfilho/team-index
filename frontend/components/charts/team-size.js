fd02e344ddfc7ccb0c9825b841b8542d48af297e
Josh Johnston <josh@x-team.com>
2015-04-22 14:49:32 +1000

    added placeholder team size chart (closes #12)

--- frontend/components/charts/team-size.js
+++ frontend/components/charts/team-size.js
@@ -1,5 +1,65 @@
 var React = require('react');
 
+var Highchart = React.createClass({
+  render: function () {
+    return <div id="team-size-chart"></div>;
+  },
+
+  shouldComponentUpdate: function (props) {
+    return false;
+  },
+
+  componentDidMount: function () {
+    new Highcharts.Chart({
+      chart: {
+        renderTo: 'team-size-chart',
+        type: 'area'
+      },
+      title: {
+        text: ''
+      },
+      xAxis: {
+        tickmarkPlacement: 'on',
+        title: {
+          enabled: false
+        }
+      },
+      yAxis: {
+        title: {
+          text: '# of Devs'
+        }
+      },
+      plotOptions: {
+        area: {
+          stacking: 'normal',
+          lineColor: '#666666',
+          lineWidth: 1,
+          marker: {
+            lineWidth: 1,
+            lineColor: '#666666'
+          }
+        }
+      },
+      series: [{
+        name: 'Gamecrate',
+        data: [3, 5, 8, 8, 8, 5, 5]
+      }, {
+        name: 'Tumbleplay',
+        data: [3, 5, 5, 5, 7, 8, 10]
+      }, {
+        name: 'Black Mnt',
+        data: [1, 1, 3, 3, 3, 3, 3]
+      }, {
+        name: 'Fox',
+        data: [6, 7, 8, 7, 10, 7, 5]
+      }, {
+        name: 'Cool Project, Bro',
+        data: [2, 2, 4, 4, 2, 5, 4]
+      }]
+    });
+  }
+});
+
 module.exports = React.createClass({
   render: function () {
     return (
@@ -9,7 +69,7 @@ module.exports = React.createClass({
         From: <input name="from" />
         To: <input name="to" />
 
-        <div className="chart"></div>
+        <div className="chart"><Highchart /></div>
       </div>
     );
   }
