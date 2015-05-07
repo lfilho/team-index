7ba8741659ef91026cf42c8d7a78b29307759ae7
Josh Johnston <josh@x-team.com>
2015-05-07 12:12:26 +1000

    added favicon

--- routes/static.js
+++ routes/static.js
@@ -1,14 +1,21 @@
 'use strict';
 
-let st = require('st');
+const st = require('st');
 
 function addRoutes (router, config) {
-  var baseUri = '/static';
-  router.addRoute(baseUri + '/*', st({
+  const baseUri = '/static';
+  const serveStaticFile = st({
     path: config.dir,
     cache: config.cacheEnabled,
     url: baseUri
-  }));
+  });
+
+  router.addRoute(baseUri + '/*', serveStaticFile);
+
+  router.addRoute('/favicon.ico', function (req, res) {
+    req.url = '/static/favicon.ico';
+    return serveStaticFile(req, res);
+  });
 }
 
 module.exports = addRoutes;
